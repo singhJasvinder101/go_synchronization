87e9920ae79d9305d2fe56d2be4040757ef55711
Jasvinder SIngh <mrkaransingh3593@gmail.com>
2025-04-11 00:04:42 +0530

    init

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -61,43 +62,58 @@ func worker(done chan bool) {
 func main() {
 	done := make(chan bool) // unbuffered
 
-	for i := 0; i < 3; i++ {
-		go worker(done) 
+	var wg sync.WaitGroup
+	// wg.Add(2)
+	wg.Add(3) 	// wait for > recievers
+
+	for i := 0; i < 5; i++ {
+		go func ()  {
+			worker(done) 
+			wg.Done()
+		}()
 	}
 
+	<-done 
+	<-done 
 
-	<-done 
-	<-done 
+	// TWIST:
+	// IF I use waitgroup for this channel to wait for 6 goroutines
+	// then I will get error 
+	
+	wg.Wait() // wait for specified number of goroutines to finish
+
+
 	// print 2	times "done" only becoz receiver nhi ha to 3,4,5th
 	// senders will block
 
-	for i := 0; i < 10; i++ {
-		// launching 10 goroutines
-		// But they all share the same variable i (which is declared 
-		// outside the goroutine) so they may conflic or either use
-		// mutex
 
-		go fmt.Println(i)
-	}
+	// for i := 0; i < 10; i++ {
+	// 	// launching 10 goroutines
+	// 	// But they all share the same variable i (which is declared 
+	// 	// outside the goroutine) so they may conflic or either use
+	// 	// mutex (instead of blocking it queues the goroutines interfering for synchronization)
 
-	for i := 0; i < 10; i++ {
-		// calling an immediately-invoked function (i) inside the 
-		// goroutine. This function receives the current i as a 
-		// copy (n), passed by value. So each goroutine prints its
-		//  own copy of the loop variable i.
+	// 	go fmt.Println(i)
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	// calling an immediately-invoked function (i) inside the 
+	// 	// goroutine. This function receives the current i as a 
+	// 	// copy (n), passed by value. So each goroutine prints its
+	// 	//  own copy of the loop variable i.
 
 
-		go func(n int) {
-			fmt.Println(n)
-		}(i)
-	}
+	// 	go func(n int) {
+	// 		fmt.Println(n)
+	// 	}(i)
+	// }
 
-	ch := make(chan int)
+	// ch := make(chan int)
 
-	select {
-	case <-ch: // blocked forever
-		fmt.Println("Received")
-	}
+	// select {
+	// case <-ch: // blocked forever
+	// 	fmt.Println("Received")
+	// }
 
 
 
